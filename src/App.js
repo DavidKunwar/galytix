529cd8fc24ec1647f7646405754e4ad13b0ce489
David Kunwar <dkunwar7111@gmail.com>
2022-10-18 18:46:38 +0530

    improved again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
-import React, { useState } from 'react';
+import axios from 'axios'
+import React, { useState } from 'react'
+
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
@@ -18,8 +18,8 @@ function App() {
   }
 
   return (
-    <div className="app">
-      <div className="search">
+    <div>
+      <div>
         <input
           value={location}
           onChange={event => setLocation(event.target.value)}
@@ -27,31 +27,31 @@ function App() {
           placeholder='Enter Location'
           type="text" />
       </div>
-      <div className="container">
-        <div className="top">
-          <div className="location">
+      <div>
+        <div>
+          <div>
             <p>{data.name}</p>
           </div>
-          <div className="temp">
+          <div>
             {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
           </div>
-          <div className="description">
+          <div>
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
         {data.name !== undefined &&
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°F</p> : null}
+          <div>
+            <div>
+              {data.main ? <p>{data.main.feels_like.toFixed()}°F</p> : null}
               <p>Feels Like</p>
             </div>
-            <div className="humidity">
-              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+            <div>
+              {data.main ? <p>{data.main.humidity}%</p> : null}
               <p>Humidity</p>
             </div>
-            <div className="wind">
-              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+            <div>
+              {data.wind ? <p>{data.wind.speed.toFixed()} MPH</p> : null}
               <p>Wind Speed</p>
             </div>
           </div>
